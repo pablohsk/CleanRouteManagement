c2db3088bf82c4781fbdedb95b370d67ab93189b
Pablohsk <pablofidelisdias@gmail.com>
2024-03-05 11:55:05 -0300

    Lógica para atualizar coordenadas dos clientes implementada

--- my-node-project/models/clientesModel.js
+++ my-node-project/models/clientesModel.js
@@ -31,7 +31,18 @@ class Cliente {
             throw new Error('Erro ao listar clientes');
         }
     }
-
+    static async atualizarCoordenadas(id, coordenada_x, coordenada_y) {
+        try {
+            const result = await pool.query(
+                'UPDATE clientes SET coordenada_x = $1, coordenada_y = $2 WHERE id = $3 RETURNING *',
+                [coordenada_x, coordenada_y, id]
+            );
+            return result.rows[0];
+        } catch (error) {
+            console.error('Erro ao atualizar coordenadas do cliente:', error);
+            throw new Error('Erro ao atualizar coordenadas do cliente');
+        }
+    }    
 }
 
 module.exports = Cliente;

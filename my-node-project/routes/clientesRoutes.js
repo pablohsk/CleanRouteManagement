bb7e35f14274d3d58137021c805fa41c779a28e1
Pablohsk <pablofidelisdias@gmail.com>
2024-03-05 11:22:56 -0300

    Código users.js de routes criado para retornar mensagens indicativas

--- my-node-project/routes/clientesRoutes.js
+++ my-node-project/routes/clientesRoutes.js
@@ -5,5 +5,6 @@ const clientesController = require('../controllers/clientesController');
 router.get('/', clientesController.listarClientes);
 router.post('/', clientesController.cadastrarCliente);
 router.put('/:id', clientesController.atualizarCoordenadas);
+router.get('/rota', clientesController.calcularRota);
 
 module.exports = router;

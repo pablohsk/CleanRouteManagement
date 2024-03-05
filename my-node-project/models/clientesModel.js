7588889d761e8792a82997b116d29da1eed53365
Pablohsk <pablofidelisdias@gmail.com>
2024-03-05 11:54:36 -0300

    Lógica para listar todos os clientes implementada

--- my-node-project/models/clientesModel.js
+++ my-node-project/models/clientesModel.js
@@ -22,7 +22,16 @@ class Cliente {
             throw new Error('Erro ao cadastrar cliente');
         }
     }
- 
+    static async listarTodos() {
+        try {
+            const result = await pool.query('SELECT * FROM clientes');
+            return result.rows;
+        } catch (error) {
+            console.error('Erro ao listar clientes:', error);
+            throw new Error('Erro ao listar clientes');
+        }
+    }
+
 }
 
 module.exports = Cliente;
